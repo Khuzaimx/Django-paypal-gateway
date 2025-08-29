5907d36354cf75f28f302bfa345f57cfe5f39976
Muhammad Khuzaim <huzaimthegenius1234@gmail.com>
2025-08-29 22:48:55 +0500

    crsf

--- store/views.py
+++ store/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Product
+from .models import Product, Category
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
@@ -53,5 +53,21 @@ def register_user(request):
             messages.success(request, "OOps we got problem plz try again..")
             return redirect('register')
     return render(request, 'store/register.html', {'form' : form})
+
+def product(request, pk):
+    product=Product.objects.get(id=pk)
+    return render(request, 'store/product.html', {'product' : product})
+
+
+def category(request, foo):
+    foo=foo.replace('-', ' ')
+    try:
+        category=Category.objects.get(name=foo)
+        products=Product.objects.filter(category=category)
+        return render(request, 'store/category.html', {'products' : products})
+    except:
+          messages.success(request, "OOps no item found in the dpecific category..")  
+
+
 
     
